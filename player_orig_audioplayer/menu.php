deeca06df85af74aa5304f93610634d631f1e17e
Scott <scottsaunders@mac.com>
2021-08-21 16:36:07 -0400

    added original audio js player as a backup

---
+++ player_orig_audioplayer/menu.php
@@ -0,0 +1,9 @@
+ <div class="main-menu">
+		  <div class="list audio-menu">
+		  	<?php foreach ($menuItems as $menuItem) : ?>
+				<div class="menu-link"><a href="/player/?series=<?php echo $menuItem->link; ?>"><i class="material-icons"> chevron_right </i></a> <?php echo $menuItem->title; ?> </div>
+			<?php endforeach; ?>
+			<div class="menu-footer"><img class="footer-img" src="/player/images/radio_footer_dark.svg" alt="radio_footer_dark" ></div>
+			<div class="menu-title">MAIN MENU - Series: <?php echo count($menuItems);?><span class="close-btn"><i class="material-icons">close</i></span></div>
+		  </div>
+	  </div>
