666f94975a68899f1ce902a4b069f29c09d464cd
Scott Saunders <scott.saunders@Saunders-S.local>
2022-04-04 18:50:33 -0400

    added php error checking to index.php files.

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 
 <?php
-		
+		ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
 	
 		if(strpos($_SERVER['REQUEST_URI'], 'menu') !== false){
 		   $menu = $_GET['menu']; 
